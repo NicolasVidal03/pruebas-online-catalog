7df60817aa6a0f0e115cb52f6e200161c746cae7
Belen <beckiar23@gmail.com>
2024-12-01 01:27:16 -0400

    Se añadio el feature del billing

--- features/homePage.feature
+++ features/homePage.feature
@@ -2,10 +2,8 @@ Feature: As a internet user
          I want to see the main buttons on the homepage
          So I can navigate to the respective sections
 
-Background:
-    Given I am on the GMO homepage
-
 Scenario: Viewing the buttons on the homepage
+    Given I am on the GMO homepage
     Then I should see the following buttons:
       | Enter GMO OnLine       |
       | About The GMO Site     |
---
+++ features/step_definitions/billingSteps.rb
@@ -0,0 +1,136 @@
+# And I set the quantity input to "1" for the "3 Person Dome Tent"
+And(/^I set the quantity input to "([^"]*)" for the "([^"]*)"$/) do |quantity, product_name|
+    # Encuentra la fila del producto
+    product_row = all('tr').find do |row|
+      row.all('td')[1]&.text&.include?(product_name)
+    end
+  
+    raise "Product row not found for #{product_name}" if product_row.nil?
+  
+    # Extrae el texto del precio unitario
+    within(product_row) do
+      price_text = all('td')[2]&.text # Ajusta el índice de 'td' según la posición del precio
+      raise "Price text not found for #{product_name}" if price_text.nil?
+  
+      unit_price = price_text.match(/\$ ([\d,]+\.\d{2})/)[1].to_f
+  
+      # Calcula el total
+      @quantity = quantity.to_i
+      @total_price = unit_price * @quantity
+  
+      # Establece la cantidad en el campo de texto
+      all('input[type="text"]')[0].set(quantity)
+    end
+  end  
+
+# Then I should see the billing form
+Then(/^I should see the billing form$/) do
+    bill_inputs = all("input") 
+    expected_bill_inputs = ["billName", "billAddress", "billCity", "billState", "billZipCode", "billPhone", "billEmail", "CardNumber", "CardDate","shipName", "shipAddress", "shipCity", "shipState", "shipZipCode", "shipPhone"]
+    
+    expected_bill_inputs.each do |name|
+      input = bill_inputs.find { |input| input[:name] == name }
+      expect(input).to be_visible
+    end
+  end
+  
+# When I fill in the billing information
+When(/^I fill in the billing information:$/) do |table|
+    table.hashes.each do |row|
+  
+      if row['Field'] == 'Name'
+        @bill_name = row['Value']
+        find('[name="billName"]').set(row['Value'])
+      elsif row['Field'] == 'Address'
+        @bill_address = row['Value']
+        find('[name="billAddress"]').set(row['Value'])
+      elsif row['Field'] == 'City'
+        @bill_city = row['Value']
+        find('[name="billCity"]').set(row['Value'])
+      elsif row['Field'] == 'State'
+        @bill_state = row['Value']
+        find('[name="billState"]').set(row['Value'])
+      elsif row['Field'] == 'Zip'
+        @bill_zip_code = row['Value']
+        find('[name="billZipCode"]').set(row['Value'])
+      elsif row['Field'] == 'Phone'
+        @bill_phone = row['Value']
+        find('[name="billPhone"]').set(row['Value'])
+      elsif row['Field'] == 'E-mail'
+        @bill_email = row['Value']
+        find('[name="billEmail"]').set(row['Value'])
+      elsif row['Field'] == 'Credit Card'
+        @card_type = row['Value']
+        find('[name="CardType"]').select(row['Value'])
+      elsif row['Field'] == 'Card Number'
+        @card_number = row['Value']
+        find('[name="CardNumber"]').set(row['Value'])
+      elsif row['Field'] == 'Expiration Date'
+        @card_date = row['Value']
+        find('[name="CardDate"]').set(row['Value'])
+      end
+    end
+  end
+
+# And I click on the "Place The Order" button
+  And(/^I fill in the shipping information:$/) do |table|
+    table.hashes.each do |row|
+  
+      if row['Field'] == 'Name'
+        @ship_name = row['Value']
+        find('[name="shipName"]').set(row['Value'])
+      elsif row['Field'] == 'Address'
+        @ship_addres = row['Value']
+        find('[name="shipAddress"]').set(row['Value'])
+      elsif row['Field'] == 'City'
+        @ship_city = row['Value']
+        find('[name="shipCity"]').set(row['Value'])
+      elsif row['Field'] == 'State'
+        @ship_state = row['Value']
+        find('[name="shipState"]').set(row['Value'])
+      elsif row['Field'] == 'Zip'
+        @ship_zip_code = row['Value']
+        find('[name="shipZipCode"]').set(row['Value'])
+      elsif row['Field'] == 'Phone'
+        @ship_phone = row['Value']
+        find('[name="shipPhone"]').set(row['Value'])
+      end
+    end
+  end
+
+  # Then I should see the order receipt details
+  Then(/^I should see the order receipt details$/) do
+    @sales_tax = (@total_price * 0.05).round(2)
+    @shipping_cost ||= 5.0 
+  
+    grand_total = (@total_price + @sales_tax + @shipping_cost).round(2)
+  
+    expect(page).to have_content("Product Total $ #{@total_price.round(2)}")
+    expect(page).to have_content("Sales Tax $ #{@sales_tax}")
+    expect(page).to have_content("Shipping & Handling $ #{@shipping_cost}")
+    expect(page).to have_content("Grand Total $ #{grand_total}")
+  end
+
+# Then I should see an error alert with the message "<AlertMessage>"
+Then(/^I should see an error alert with the message "(.*?)"$/) do |alert|
+    alert_text = page.driver.browser.switch_to.alert.text
+    expect(alert_text.strip).to eq(alert)
+  end
+
+# And I click on the "Same as 'Bill To'" checkbox
+And(/^I click on the "Same as 'Bill To'" checkbox$/) do
+    find('input[name="shipSameAsBill"]').click
+end
+  
+# Then the shipping information should be automatically filled with the following:
+Then(/^the shipping information should be automatically filled with the following:$/) do |table|
+    table.hashes.each do |row|
+      field_name = row['Field']
+      expected_value = row['Value']
+  
+      # Verifica que el campo de envío tiene el valor esperado
+      actual_value = find("[name='ship#{field_name}']").value
+      expect(actual_value).to eq(expected_value), "Expected #{field_name} to be '#{expected_value}', but got '#{actual_value}'"
+    end
+  end
+  
