0fe3cfb5b507ad8e20e26f5d656301164efcc2ad
Belen <beckiar23@gmail.com>
2024-12-03 23:37:57 -0400

    Se añadierons 2 hooks de apoyo

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,3 +1,17 @@
 After do 
     Capybara.current_session.driver.quit
 end
+
+AfterStep do |step|
+    if step.failed?
+      save_screenshot("screenshots/#{step.name}.png")
+    end
+  end
+
+Around do |scenario, block|
+    start_time = Time.now
+    block.call
+    end_time = Time.now
+    puts "El escenario '#{scenario.name}' tomó #{end_time - start_time} segundos."
+end
+  
