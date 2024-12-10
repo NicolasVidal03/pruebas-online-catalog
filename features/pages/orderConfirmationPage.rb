93a5963e2158b3f151a6b13b1333ff763ebd9e18
Belen <beckiar23@gmail.com>
2024-12-09 22:05:29 -0400

    Se refactorizo el feature bigOrder para permitir la integracion del page object pattern

---
+++ features/pages/orderConfirmationPage.rb
@@ -0,0 +1,42 @@
+class OrderConfirmationPage
+    include Capybara::DSL
+    include RSpec::Matchers
+  
+    def verify_order_details(table)
+        order_details = page.find('body > form > table > tbody > tr:nth-child(1) > td > div > center > table > tbody')
+  
+        table.hashes.each_with_index do | row, index |
+            qty = row['Qty']
+            description = row['Product Description']
+            delivery = row['Delivery Status']
+            unit_price = row['Unit Price']
+            total_price = row['Total Price']
+
+        row_index = index + 2
+        order_row = order_details.find("tr:nth-child(#{row_index})")
+
+        expect(order_row.find('td:nth-child(1)').text).to eq(qty)
+        expect(order_row.find('td:nth-child(2)').text).to eq(description)
+        expect(order_row.find('td:nth-child(3)').text).to eq(delivery)
+        expect(order_row.find('td:nth-child(4)').text).to eq(unit_price)
+        expect(order_row.find('td:nth-child(5)').text).to eq(total_price)
+    end
+
+    def verify_additional_info(table)
+        info_order = page.find('body > form > table > tbody > tr:nth-child(1) > td > div > center > table > tbody')
+        table.rows.each do |row|
+            info_label = row[0]
+            info_value = row[1]
+            info_row = info_order.find('tr', text: /\b#{info_label}\b/i)
+            expect(info_row).to have_content(info_label)
+            value_element = nil
+            if info_label == 'Product Total'
+                value_element = info_row.find('td:nth-child(3)')
+            else
+                value_element = info_row.find('td:nth-child(2)')
+            end
+            expect(value_element).to have_content(info_value)
+        end
+    end
+end
+end
